b387670ee3fdc02e19f1099ad680e1a26b12206f
Andrey Lapin <nethack@mail.ru>
2022-09-03 15:33:49 +0400

    Finished regex

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,28 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
-
+  containsNumber: function (str) {
+    return /\d/gm.test(str);
   },
 
-  containsRepeatingLetter: function(str) {
-
+  containsRepeatingLetter: function (str) {
+    return /([a-zA-Z])\1/.test(str);
   },
 
-  endsWithVowel: function(str) {
-
+  endsWithVowel: function (str) {
+    return /[aeiou]$/gim.test(str);
   },
 
-  captureThreeNumbers: function(str) {
-
+  captureThreeNumbers: function (str) {
+    console.log(str.match(/\d{3}/im));
+    return str.match(/\d{3}/gim) ? str.match(/\d{3}/gim)[0] : false;
   },
 
-  matchesPattern: function(str) {
-
+  matchesPattern: function (str) {
+    return /^\d{3}-\d{3}-\d{4}$/gim.test(str);
   },
 
-  isUSD: function(str) {
-
-  }
+  isUSD: function (str) {
+    return /^\$(\d{1,3}){1}(\,)*(\d{3}\,)*(\d{3})*(\.\d{2})?$/gim.test(str);
+  },
 };
