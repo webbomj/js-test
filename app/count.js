d275d6ec66fcea139c7bbcefe7028bf767574f74
Andrey Lapin <nethack@mail.ru>
2022-09-04 01:40:34 +0400

    Finished count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,23 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let timeout;
 
-  }
+    function counter() {
+      if (start <= end) {
+        console.log(start);
+        timeout = setTimeout(counter, 100);
+        start += 1;
+      }
+    }
+
+    counter();
+
+    return {
+      cancel: function () {
+        clearTimeout(timeout);
+      },
+    };
+  },
 };
