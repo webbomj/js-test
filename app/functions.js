2b9077277a7cbdb8bd49cb79688fd3fc3730084a
Andrey Lapin <nethack@mail.ru>
2022-09-03 22:01:55 +0400

    Finished: functions, flowControl, and 2/4 recursion

--- app/functions.js
+++ app/functions.js
@@ -1,21 +1,65 @@
 exports = typeof window === "undefined" ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function (fn, arr) {},
+  argsAsArray: function (fn, arr) {
+    return fn.apply(null, arr);
+  },
 
-  speak: function (fn, obj) {},
+  speak: function (fn, obj) {
+    return fn.apply(obj);
+  },
 
-  functionFunction: function (str) {},
+  functionFunction: function (str) {
+    return (str2) => {
+      return `${str}, ${str2}`;
+    };
+  },
 
-  makeClosures: function (arr, fn) {},
+  makeClosures: function (arr, fn) {
+    return arr.map((el) => {
+      return fn.bind(null, el);
+    });
+  },
 
-  partial: function (fn, str1, str2) {},
+  partial: function (fn, str1, str2) {
+    const result = (a) => fn.call(null, str1, str2, a);
+    return result;
+  },
 
-  useArguments: function () {},
+  useArguments: function () {
+    const arg = Array.from(arguments);
+    return arg.reduce((acc, el) => acc + el, 0);
+  },
 
-  callIt: function (fn) {},
+  callIt: function (fn) {
+    const arg = Array.from(arguments);
+    arg.shift();
+    return fn.apply(null, arg);
+  },
 
-  partialUsingArguments: function (fn) {},
+  partialUsingArguments: function (fn) {
+    const arg = [];
 
-  curryIt: function (fn) {},
+    if (arguments.length !== 1) {
+      const argOuter = Array.from(arguments);
+      argOuter.forEach((el) => arg.push(el));
+      arg.shift();
+    }
+
+    return function () {
+      const argInner = Array.from(arguments);
+      argInner.forEach((el) => arg.push(el));
+      return fn.apply(null, arg);
+    };
+  },
+
+  curryIt: function (fn) {
+    return function (a) {
+      return function (b) {
+        return function (c) {
+          return fn.call(null, a, b, c);
+        };
+      };
+    };
+  },
 };
--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,41 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
+    let result = [];
 
+    function recursionData(data) {
+      return data.files.forEach((el) => {
+        if (el.files) {
+          recursionData(el);
+        } else {
+          if (dirName) {
+            const extension = el.split(".");
+
+            if (extension[1] === dirName) {
+              result.push(el);
+            }
+          } else {
+            result.push(el);
+          }
+        }
+      });
+    }
+
+    recursionData(data);
+
+    return result;
   },
 
-  permute: function(arr) {
+  permute: function (arr) {},
 
+  fibonacci: function (n) {
+    if (n === 1 || n === 0) {
+      return n;
+    }
+
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
   },
 
-  fibonacci: function(n) {
-
-  },
-
-  validParentheses: function(n) {
-
-  }
+  validParentheses: function (n) {},
 };
